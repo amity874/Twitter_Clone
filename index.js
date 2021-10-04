d81b018bd14bef67f1facbbcb22e6d92000e4944
Amit <amity552000@gmail.com>
2021-10-04 16:38:57 +0530

    Css html img file connected

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express =require('express');
 const router=require('./src/routes/index');
 var expressLayouts=require('express-ejs-layouts');
 const app=express();
+app.use(express.static(__dirname+'/src/assests'));
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 app.use(expressLayouts);
 app.set('layout',__dirname+'/src/views/layouts/layout');
 app.set('view engine','ejs');
-app.set('views','./src/views');
+app.set('views','./src/views')
+
+
+
 app.use('/',router);
 app.listen(3000,()=>{
     console.log('Server Started At 3000!!');
