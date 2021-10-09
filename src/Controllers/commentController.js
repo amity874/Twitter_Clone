bb05505dd404601f3f9a010458f8fc8bc5197574
Amit <amity552000@gmail.com>
2021-10-09 15:39:27 +0530

    Segrigation

--- src/Controllers/commentController.js
+++ src/Controllers/commentController.js
@@ -1,5 +1,6 @@
 const Comment=require('../models/comment');
 const Tweet=require('../models/tweet');
+const { param } = require('../routes');
 const create =function(req,res){
     Tweet.findById(req.body.tweet,function(err,tweet){
         console.log(req.body);
@@ -23,4 +24,21 @@ const create =function(req,res){
         }
     })
 }
-module.exports={create};
+const destroy = function(req, res) {
+    // console.log(req);
+    Comment.findById(req.params.id, function(err, comment) {
+        if(!comment) {
+            return res.redirect('back');
+        }
+        if(comment.user == req.user.id) {
+            let tweetId = comment.tweet;
+            comment.remove();
+            Tweet.findByIdAndUpdate(tweetId, { $pull: {comments: req.params.id}} , function(err, tweet) {
+                return res.redirect('back');
+            })
+        } else {
+            return res.redirect('back');
+        }
+    });
+}
+module.exports={create,destroy};
--- src/routes/comments.js
+++ src/routes/comments.js
@@ -1,7 +1,8 @@
 const express=require('express');
 const passport=require('passport');
-const {create}=require('../Controllers/commentController');
+const {create,destroy}=require('../Controllers/commentController');
 const router=express.Router();
 // router.post('/create',create);
 router.post('/create',passport.checkAuthentication,create);
+router.get('/destroy/:id',passport.checkAuthentication,destroy);
 module.exports=router; 
